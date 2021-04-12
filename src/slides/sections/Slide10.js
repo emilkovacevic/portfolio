ec9927e1e1c3d342114f6952da7193a1ac6da9a7
emilthedev <emil.kovacevic.dev@gmail.com>
2021-04-12 21:23:47 +0200

    Update Slide10.js

--- src/slides/sections/Slide10.js
+++ src/slides/sections/Slide10.js
@@ -1,9 +1,9 @@
-import React,{useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import ErrorFlash from './helpers/ErrorFlash';
-import {personal} from '../../data/data'
+import { personal } from '../../data/data'
 import ContactForm from '../../components/contact/ContactForm'
 
 const Component = styled.div`
@@ -95,15 +95,15 @@ function Slide10() {
 
     const flashDelay = 1.5;
     const [flash, setFlash] = useState(true)
-  
+
     useEffect(() => {
-      let flashTimer = setTimeout(() => setFlash(false), flashDelay * 1000);
-      return () => {
-        clearTimeout(flashTimer);
-      };
+        let flashTimer = setTimeout(() => setFlash(false), flashDelay * 1000);
+        return () => {
+            clearTimeout(flashTimer);
+        };
     }, []);
 
-    const [form, setForm ] = useState(false)
+    const [form, setForm] = useState(false)
 
     const handleForm = (e) => {
         e.preventDefault()
@@ -112,45 +112,47 @@ function Slide10() {
 
     return (
         <>
-        {flash ? 
-            <ErrorWrapper>
-                <ErrorFlash />
-            </ErrorWrapper> 
-        :
-        <Component>
-            <h2>Oh No!</h2>
-            <h2>Looks like the presentation is over :(</h2>
-            <p>But there are some buttons below to explore</p>
-            <br/>
-            <br/>
-            <button onClick={handleForm}>Contact Me</button>
-            <h1
-            style={{
-            borderBottom: "1px solid var(--text)",
-            margin:"1em 0 1em 0"}}
-            >
-            Site Map</h1>
-           <div>
-               <div className="links">
-               <div className='routes'>
-                   <a href={personal.github.link} target="_blank" rel="noreferrer"><div><i className="fab fa-github-square"/>Github</div></a>
-                   <a href={personal.linkedin.link} target="_blank" rel="noreferrer"><div><i className="fab fa-linkedin"/>Linked In</div></a>
-                   <a href={"mailto:" + personal.mail.link} target="_blank" rel="noreferrer"><div> <i className="fas fa-envelope-open-text"/>E-mail </div></a>
-                </div>
-                <div className='routes'>
-                   <Link to='/projects'><i className="fas fa-hammer"></i>Projects</Link>
-                   <Link to='/about'><i className="fas fa-user"></i>About Me</Link>
-                   <Link to='/cv'><i className="far fa-file"></i>CV</Link>
-               </div>
-               </div>
-           </div>
-           {form && <ContactForm onClick={setForm}></ContactForm>}
-           <h5>The website source code =<a href="https://github.com/emilkovacevic/portfolio" target="_blank" rel="noreferrer">#Code</a></h5>
-           <p>*The error event was an intentional design choice*</p>
-           <br/>
-           <a href="http://emilkovacevic.com/">reset</a>
-        </Component>
-        }
+            {flash ?
+                <ErrorWrapper>
+                    <ErrorFlash />
+                </ErrorWrapper>
+                :
+                <Component>
+                    <h2>Oh No!</h2>
+                    <h2>Looks like the presentation is over :(</h2>
+                    <p>But there are some buttons below to explore</p>
+                    <div>
+                        <br/>
+                        <button onClick={handleForm}>Contact Me</button>
+                    </div>
+                    <h1
+                        style={{
+                            borderBottom: "1px solid var(--text)",
+                            margin: "1em 0 1em 0"
+                        }}
+                    >
+                        Site Map</h1>
+                    <div>
+                        <div className="links">
+                            <div className='routes'>
+                                <a href={personal.github.link} target="_blank" rel="noreferrer"><div><i className="fab fa-github-square" />Github</div></a>
+                                <a href={personal.linkedin.link} target="_blank" rel="noreferrer"><div><i className="fab fa-linkedin" />Linked In</div></a>
+                                <a href={"mailto:" + personal.mail.link} target="_blank" rel="noreferrer"><div> <i className="fas fa-envelope-open-text" />E-mail </div></a>
+                            </div>
+                            <div className='routes'>
+                                <Link to='/projects'><i className="fas fa-hammer"></i>Projects</Link>
+                                <Link to='/about'><i className="fas fa-user"></i>About Me</Link>
+                                <Link to='/cv'><i className="far fa-file"></i>CV</Link>
+                            </div>
+                        </div>
+                    </div>
+                    {form && <ContactForm onClick={setForm}></ContactForm>}
+                    <h5>The website source code =<a href="https://github.com/emilkovacevic/portfolio" target="_blank" rel="noreferrer">#Code</a></h5>
+                    <p>*The error event was an intentional design choice*</p>
+                    <br />
+                    <a href="http://emilkovacevic.com/">reset</a>
+                </Component>
+            }
         </>
     )
 }
